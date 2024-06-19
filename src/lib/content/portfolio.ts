462168b4065a884349d70d5e2fca256f6e0e9646
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-06-19 19:47:35 +0530

    Initial Commit

---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,39 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: '',
+  instagram: '',
+  twitter: '',
+  github: 'https://github.com/SuryanshBVerma',
+  linkedin: 'www.linkedin.com/in/suryansh-bachchan',
+};
+
+export const author = {
+  name: 'Suryansh Bachchan Verma',
+  email: 'suryanshbverma@gmail.com',
+};
+
+export const seoData = {
+  title: 'Suryansh Bachchan Verma | Full Stack Developer',
+  description:
+    'Suryansh Bachchan Verma is a Full Stack developer who specializes in building web apps',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://vatsalsinghkv.vercel.app/',
+  keywords: [
+    'Suryansh',
+    'Suryansh Bachchan Verma',
+    '@suryanshbverma',
+    'sbv',
+    'Portfolio',
+    'Suryansh Portfolio ',
+    'Suryansh Bachchan Verma Portfolio',
+  ],
+};
---
+++ tailwind.config.ts
@@ -0,0 +1,70 @@
+const svgToDataUri = require("mini-svg-data-uri");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        signature: ['agustina', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+        mono: ['Roboto Mono', 'monospace'],
+      },
+      screens: {
+        xs: '375px',
+      },
+      colors: {
+        bg: 'var(--color-bg)',
+        'bg-secondary': 'var(--color-bg-secondary)',
+        accent: 'var(--color-accent)',
+        'accent-light': 'var(--color-accent-light)',
+        text: 'var(--color-text)',
+        'dark-1': 'var(--color-dark-1)',
+        'dark-2': 'var(--color-dark-2)',
+        'dark-3': 'var(--color-dark-3)',
+      },
+      transitionTimingFunction: {
+        'in-scroll': 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+      },
+      gridTemplateColumns: {
+        'auto-300': 'repeat(auto-fill, minmax(300px, 1fr))',
+        'auto-250': 'repeat(auto-fill, minmax(250px, 1fr))',
+      },
+      backgroundImage: {
+        torch:
+          'radial-gradient(525px at 50% 50%, var(--color-accent-torch), transparent 80%)',
+      },
+    },
+  },
+  plugins: [
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
+};
