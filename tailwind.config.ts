7f4292648f16197f59ac685e7b30bd1ed505f075
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-06-19 20:30:01 +0530

    tailwind changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
-const svgToDataUri = require("mini-svg-data-uri");
- 
-const colors = require("tailwindcss/colors");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+import svgToDataUri from "mini-svg-data-uri";
+import colors from "tailwindcss/colors";
+import "./tailwindcss.d.ts"
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
 
 /** @type {import('tailwindcss').Config} */
